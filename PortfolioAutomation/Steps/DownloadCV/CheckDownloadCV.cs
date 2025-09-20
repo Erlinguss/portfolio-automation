a557c68be508bb414c9b5def9c16782381f859d3
Erlinguss <D00251914@student.dkit.ie>
2025-09-20 21:55:04 +0100

    Add Download CV test case
    - Implemented DownloadCVAssertion to validate CV link opens a PDF
    - Updated test flow to handle navigation in the same tab (instead of popup)
    - Added CheckDownloadCV step for reusability
    - Created DownloadCVButtonTest to integrate step + assertion

---
+++ PortfolioAutomation/Steps/DownloadCV/CheckDownloadCV.cs
@@ -0,0 +1,26 @@
+﻿using PortfolioAutomation.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace PortfolioAutomation.Steps.DownloadCV
+{
+    public class CheckDownloadCV : BaseStep
+    {
+        public CheckDownloadCV(IRepository repository, IStep next = null)
+            : base(repository, next) { }
+
+        protected override async Task PerformExecute()
+        {
+            var page = Repository.Get<IPage>();
+            Console.WriteLine("Clicking Download CV link...");
+
+            // Click the CV download link
+            await page.ClickAsync("a:has-text('Download CV')");
+            await page.WaitForTimeoutAsync(1000);
+        }
+    }
+}
